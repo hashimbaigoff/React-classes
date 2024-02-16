51dff954a2a866b949b430ec7eab1b6dad56af16
Hashim Baig <hashimbaigoff@gmail.com>
2024-02-16 12:22:14 +0530

    add: 16-02-2024

---
+++ src/Components/ContentAbout/index.js
@@ -0,0 +1,9 @@
+function Content() {
+    return (
+        <div className="container">
+            <h1>About us content</h1>            
+        </div>
+    )
+}
+
+export default Content;
---
+++ src/Pages/Contact/index.js
@@ -0,0 +1,15 @@
+import Footer from "../../Components/Footer";
+import Header from "../../Components/Header";
+import Slider from "../../Components/Slider";
+
+function Contact() {
+    return (
+        <>
+        <Header />
+        <Slider  details="kousar"/>
+        <Footer />
+        </>
+    )
+}
+
+export default Contact;
